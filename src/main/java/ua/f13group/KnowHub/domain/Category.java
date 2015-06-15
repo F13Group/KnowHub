f90a5e9736e98a1c431d9609e81a6b6432c853be
Alona_Pishchulova <pischulova@gmail.com>
2015-06-15 17:01:58 +0300

    model entities added

---
+++ src/main/java/ua/f13group/KnowHub/domain/Category.java
@@ -0,0 +1,33 @@
+package ua.f13group.KnowHub.domain;
+
+public class Category {
+	Long id;
+	String value;
+	Long postId;
+	
+	public Category() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Long getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+}
---
+++ src/main/java/ua/f13group/KnowHub/domain/Tag.java
@@ -0,0 +1,33 @@
+package ua.f13group.KnowHub.domain;
+
+public class Tag {
+	Long id;
+	String value;
+	Long questionId;
+	
+	public Tag() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Long getQuestionId() {
+		return questionId;
+	}
+
+	public void setQuestionId(Long questionId) {
+		this.questionId = questionId;
+	}
+}
