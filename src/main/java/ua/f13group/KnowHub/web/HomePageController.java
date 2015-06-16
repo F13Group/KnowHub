7fb468353846193580e6ed4012ef8c05d330cfbe
IvanParovoy <vanya.yarovoy@gmail.com>
2015-06-16 17:56:36 +0300

    Added web package whith HomePageController.java

---
+++ src/main/java/ua/f13group/KnowHub/web/HomePageController.java
@@ -0,0 +1,17 @@
+package ua.f13group.KnowHub.web;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping({ "/" })
+public class HomePageController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String homePageAll(Map<String, Object> model) {
+		return "homePage";
+	}
+}
