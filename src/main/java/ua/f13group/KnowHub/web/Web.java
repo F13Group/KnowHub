522352029baaa301f8894ec2e9ea079b4b157f2c
Chronoss93 <IgorKravchenko93@gmail.com>
2015-06-16 14:40:33 +0300

    test

--- src/main/java/ua/f13group/KnowHub/web/Web.java
+++ src/main/java/ua/f13group/KnowHub/web/Web.java
@@ -14,8 +14,6 @@ public class Web {
 	@RequestMapping("/hello")
 	public @ResponseBody String showMessage(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		System.out.println("in controller");
-
 		return message;
 	}
 }
