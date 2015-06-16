e0a4c35292545d2c83cb0a52475831717aabec3a
IvanParovoy <vanya.yarovoy@gmail.com>
2015-06-16 15:59:20 +0300

    Added more records to SimpleServiceImplementation

--- src/main/java/ua/f13group/KnowHub/service/simpleImplementation/CategoryServiceSimpleImplementation.java
+++ src/main/java/ua/f13group/KnowHub/service/simpleImplementation/CategoryServiceSimpleImplementation.java
@@ -17,15 +17,33 @@ public class CategoryServiceSimpleImplementation implements CategoryService {
 	Category cat = new Category();
 	cat.setId(new Long(1));
 	cat.setPostId(new Long(1));
-	cat.setValue("Category1");
+	cat.setValue("All");
 	
 	Category cat2 = new Category();
 	cat2.setId(2l);
 	cat2.setPostId(2l);
-	cat2.setValue("Category2");
+	cat2.setValue("Java");
+	
+	Category cat3 = new Category();
+	cat3.setId(3l);
+	cat3.setPostId(3l);
+	cat3.setValue(".NET");
+	
+	Category cat4 = new Category();
+	cat4.setId(4l);
+	cat4.setPostId(4l);
+	cat4.setValue("Bisiness intelligence");
+	
+	Category cat5 = new Category();
+	cat5.setId(5l);
+	cat5.setPostId(5l);
+	cat5.setValue("Functional testing");
 	
 	mock.add(cat);
 	mock.add(cat2);
+	mock.add(cat3);
+	mock.add(cat4);
+	mock.add(cat5);
 	
 	return mock;
 }
--- src/main/java/ua/f13group/KnowHub/service/simpleImplementation/QuestionServiceSimpleImplementation.java
+++ src/main/java/ua/f13group/KnowHub/service/simpleImplementation/QuestionServiceSimpleImplementation.java
@@ -26,8 +26,22 @@ public class QuestionServiceSimpleImplementation implements QuestionService {
 		q2.setRating(200l);
 		q2.setValue("Question 2");
 		
+		Question q3 = new Question();
+		q3.setId(3l);
+		q3.setPostId(32l);
+		q3.setRating(300l);
+		q3.setValue("Question 3");
+		
+		Question q4 = new Question();
+		q4.setId(4l);
+		q4.setPostId(4l);
+		q4.setRating(400l);
+		q4.setValue("Question 4");
+		
 		mock.add(q1);
 		mock.add(q2);
+		mock.add(q3);
+		mock.add(q4);
 		
 		return mock;
 	}
