07da19fce00cbd1e218bca94cdd3ce13f4d49418
MarinkMak <marinkmak@gmail.com>
2015-06-20 14:50:29 +0300

    add sout to view questions in category

--- src/main/java/ua/f13group/KnowHub/web/rest/CategoryController.java
+++ src/main/java/ua/f13group/KnowHub/web/rest/CategoryController.java
@@ -9,23 +9,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import ua.f13group.KnowHub.domain.Category;
+import ua.f13group.KnowHub.domain.Question;
 import ua.f13group.KnowHub.service.CategoryService;
 
 @RestController
-@RequestMapping(value="/category")
+@RequestMapping(value = "/category")
 @ImportResource("classpath:spring/spring-config.xml")
 public class CategoryController {
-	
+
 	@Autowired
 	CategoryService categoryService;
-	
-	 @RequestMapping(method = RequestMethod.GET)
-	 public List<Category> getAllCategories() {
-	      System.out.println("HELLO!~");
-	        for(Category c:categoryService.findAll())
-	        	System.out.println(c);
-	        	  return categoryService.findAll();
-	    }
-	 
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Category> getAllCategories() {
+		System.out.println("HELLO!~");
+		for (Category c : categoryService.findAll()) {
+			System.out.println(c);
+			for(Question q: c.getQuestions()){
+				System.out.println(" - q - "+q);
+			}
+		}
+		return categoryService.findAll();
+	}
 
 }
