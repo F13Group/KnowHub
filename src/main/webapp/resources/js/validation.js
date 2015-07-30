0425055d6471bf28ed5360385d77253a2bd3c10e
Dmytro_Glomozda <Dmytro_Glomozda@epam.com>
2015-07-30 16:40:36 +0300

    several bugs fixed

--- src/main/webapp/resources/js/validation.js
+++ src/main/webapp/resources/js/validation.js
@@ -94,12 +94,21 @@ function validatePassword(message1, message2, message3) {
 	var firstname = login.substr(0, login.indexOf("_"));
 	var lastname = login.substr(login.indexOf("_")+1, login.indexOf("@")-login.indexOf("_")-1);
 	
-	if (pass1 == null || pass1 == "") {                
+	if (pass1 == null || pass1 == "") {		
 		document.getElementById("password").style.border = "1px solid #B22746";
 		document.getElementById("password").style.boxShadow = "0 0 10px #B22746";
 		$("#password-group").after("<div class='error col-lg-offset-2' id=password-error>" + message1 + "</div>");                
 		return false;
-	}	
+	}
+	
+	if (login != null && login != "" && firstname.length != 0 && lastname.length != 0) {
+		if ((pass1.toLowerCase().indexOf(firstname) !=-1) || (pass1.toLowerCase().indexOf(lastname) !=-1) || (pass1.indexOf(" ") !=-1)) {			
+			document.getElementById("password").style.border = "1px solid #B22746";
+			document.getElementById("password").style.boxShadow = "0 0 10px #B22746";                
+			$("#password-group").after("<div class='error col-lg-offset-2' id=password-error>" + message2 + "</div>");            	
+			return false;
+		}
+	}
 
 	var regexp1 = /((?=.*\d)(?=.*[a-z]).{8,})/;
 	var regexp2 = /((?=.*\d)(?=.*[A-Z]).{8,})/;
@@ -107,11 +116,9 @@ function validatePassword(message1, message2, message3) {
 	var regexp4 = /((?=.*[a-z])(?=.*[A-Z]).{8,})/;
 	var regexp5 = /((?=.*[a-z])(?=.*[~@#$%^\+\-\=\[\]\*\(\)\/\{\}\\\?\!\|\:\;\_\<\>]).{8,})/;
 	var regexp6 = /((?=.*[A-Z])(?=.*[~@#$%^\+\-\=\[\]\*\(\)\/\{\}\\\?\!\|\:\;\_\<\>]).{8,})/;  
-	if ((!regexp1.test(pass1) & !regexp2.test(pass1) & !regexp3.test(pass1) & !regexp4.test(pass1) & !regexp5.test(pass1) & !regexp6.test(pass1)) || (((pass1.toLowerCase().indexOf(firstname)) !=-1) || ((pass1.toLowerCase().indexOf(lastname)) !=-1)) || (pass1.indexOf(" ") !=-1) ) {
+	if (!regexp1.test(pass1) & !regexp2.test(pass1) & !regexp3.test(pass1) & !regexp4.test(pass1) & !regexp5.test(pass1) & !regexp6.test(pass1)) {		
 		document.getElementById("password").style.border = "1px solid #B22746";
 		document.getElementById("password").style.boxShadow = "0 0 10px #B22746";                
-		document.getElementById("password").value = "";
-		document.getElementById("password2").value = "";
 		$("#password-group").after("<div class='error col-lg-offset-2' id=password-error>" + message2 + "</div>");            	
 		return false;
 	}
@@ -119,8 +126,6 @@ function validatePassword(message1, message2, message3) {
 	if (pass1.length > 20) {
 		document.getElementById("password").style.border = "1px solid #B22746";
 		document.getElementById("password").style.boxShadow = "0 0 10px #B22746";                
-		document.getElementById("password").value = "";
-		document.getElementById("password2").value = "";
 		$("#password-group").after("<div class='error col-lg-offset-2' id=password-error>" + message3 + "</div>");            	
 		return false;
 	}
@@ -129,7 +134,6 @@ function validatePassword(message1, message2, message3) {
 }
 
 function validatePassword2(message1, message2) {	
-	
 	if ($("#password2-error")[0]) {
 		return false;
 	}
@@ -149,8 +153,6 @@ function validatePassword2(message1, message2) {
 		document.getElementById("password").style.boxShadow = "0 0 10px #B22746";
 		document.getElementById("password2").style.border = "1px solid #B22746";
 		document.getElementById("password2").style.boxShadow = "0 0 10px #B22746";
-		document.getElementById("password").value = "";
-		document.getElementById("password2").value = "";
 		$("#password2-group").after("<div class='error col-lg-offset-2' id=password2-error>" + message2 + "</div>");            	
 		return false;
 	}
