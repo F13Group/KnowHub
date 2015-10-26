ce3aea5f1f8367e8fb6347d1bfeff593b0fac932
Alex-st <samosyonok@gmail.com>
2015-10-26 17:32:12 +0200

    New column for Bookmarked questions added.

--- src/main/java/ua/f13group/KnowHub/domain/QuestionSortConfig.java
+++ src/main/java/ua/f13group/KnowHub/domain/QuestionSortConfig.java
@@ -3,7 +3,9 @@ package ua.f13group.KnowHub.domain;
 public enum QuestionSortConfig {
 	DATE("Date", "loadDate"), 
 	CATEGORY("Category","value"), 
-	RATING("Frequently asked", "rating");
+	RATING("Frequently asked", "rating"),
+	//TODO: "rating" for Bookmark should be changed on DB field name in table Questions responsible for bookmark 
+	BOOKMARK("My Bookmarks", "rating");
 	
 	private final String name;
 	public final String dbName;
--- src/main/webapp/resources/js/pagination.js
+++ src/main/webapp/resources/js/pagination.js
@@ -98,7 +98,7 @@ function outputQuestions(pC, cPN, rOPN) {
  
 		           	var userName = $("#userName").html();
 		           	if (userName) {
-		           	$("<div class='divRow row'><div class='col-lg-6 col-md-6 col-sm-6 divQuestionColor divCell_2'>" + value.value + "</div><div class='col-lg-2 col-md-2 col-sm-2 divCell_Center'>" + value.category.shortValue + "</div><div class='col-lg-2 col-md-2 col-sm-2 divCell_Center'>" + date + "</div><div class='col-lg-2 col-md-2 col-sm-2 divCell_Left'>" + was_asked_button(value.id, value.isAsked) + " " + value.rating +  "</div></div>").insertAfter("#headRow");
+		           	$("<div class='divRow row'><div class='col-lg-6 col-md-6 col-sm-6 divQuestionColor divCell_2'>" + value.value + "</div><div class='col-lg-1 col-md-2 col-sm-2 divCell_Center'>" + value.category.shortValue + "</div><div class='col-lg-1 col-md-2 col-sm-2 divCell_Center'>" + date + "</div><div class='col-lg-2 col-md-2 col-sm-2 divCell_Center'>" + was_asked_button(value.id, value.isAsked) + " " + value.rating +  "</div><div class='col-lg-2 col-md-2 col-sm-2 divCell_Center'>" + "<img id=\"was_bookmarked\" src=\"resources/img/star.png\" width=\"20\" height=\"20\"/>" + "</div></div>").insertAfter("#headRow");
 			
 		           	} else{
 		           		$("<div class='divRow row'><div class='col-lg-6 col-md-6 col-sm-6 divQuestionColor divCell_2'>" + value.value + "</div><div class='col-lg-2 col-md-2 col-sm-2 divCell_Center'>" + value.category.shortValue + "</div><div class='col-lg-2 col-md-2 col-sm-2 divCell_Center'>" + date + "</div><div class='col-lg-2 col-md-2 col-sm-2 divCell_Center'>" + value.rating +  "</div></div>").insertAfter("#headRow");
