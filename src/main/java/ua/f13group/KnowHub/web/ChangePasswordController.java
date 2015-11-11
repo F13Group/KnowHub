91eab7e3c2531824c04b6d81e383d2ba7b839df9
Dmytro_Korniienko1 <Dmytro_Korniienko1@EPUAKYIW0130.kyiv.epam.com>
2015-11-11 17:36:17 +0200

    Changed RequestMapping value parameter of ChangePasswordController method (added .jsp extension)

--- src/main/java/ua/f13group/KnowHub/web/ChangePasswordController.java
+++ src/main/java/ua/f13group/KnowHub/web/ChangePasswordController.java
@@ -2,6 +2,7 @@ package ua.f13group.KnowHub.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,15 +18,16 @@ public class ChangePasswordController {
 	@Autowired
 	UserRepository userRepository;
 	
-	@RequestMapping(value = "/restore_password/{generatedLink}")
-	public User checkReceivedLink (@PathVariable String generatedLink){
+	@RequestMapping(value = "/restore_password/{generatedLink}.jsp")
+	public String checkReceivedLink (Model model, @PathVariable String generatedLink){
 		Confirmation confirmation = userRepository.getRestorePassByLink(generatedLink);
 		if (confirmation == null){
 			return null;
 		}
 		else {
 			if (confirmation.getRegDate().getTime() + CHANGE_PASS_EXPIRATION_PERIOD > System.currentTimeMillis()){
-				return confirmation.getUser();
+				model.addAttribute("user_id", confirmation.getUser().getUserId());
+				return "reset";
 			}
 		}
 		return null;
