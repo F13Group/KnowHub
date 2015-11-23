debc63bd40c0aa235c4e4d3665c77cc945131668
Alex-st <samosyonok@gmail.com>
2015-11-23 17:14:50 +0200

    Commit with new jsp for QuestionPage

---
+++ src/main/java/ua/f13group/KnowHub/web/SingleQuestionController.java
@@ -0,0 +1,32 @@
+package ua.f13group.KnowHub.web;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ua.f13group.KnowHub.service.QuestionService;
+import ua.f13group.KnowHub.web.dto.QuestionFrequentAskedDTO;
+
+@Controller
+@RequestMapping(value = "/question")
+public class SingleQuestionController {
+	
+	@Autowired
+    QuestionService questionService;
+
+
+	@RequestMapping(value = "/{questionId}", method = RequestMethod.GET)
+    public String getQuestion(
+            @PathVariable Long questionId,
+            Principal principal) {
+	
+		return "question";
+	}
+}
