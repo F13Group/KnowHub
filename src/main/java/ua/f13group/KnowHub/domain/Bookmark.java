3b0e36f4fb9a86d4874938988f29c64e6efb69fd
Dmytro_Korniienko1 <Dmytro_Korniienko1@EPUAKYIW0130.kyiv.epam.com>
2015-12-02 18:28:27 +0200

    Working commit (current page number needs fixing)

--- src/main/java/ua/f13group/KnowHub/domain/Bookmark.java
+++ src/main/java/ua/f13group/KnowHub/domain/Bookmark.java
@@ -8,7 +8,9 @@ import javax.persistence.*;
 
 @NamedQueries({
         @NamedQuery(name = "Bookmark.isBookmarked", query = "SELECT count(b.id)>0 as my_bool FROM Bookmark b WHERE b.userId = :userId and b.questionId=:questionId"),
-        @NamedQuery(name = "Bookmark.unbookmark", query = "DELETE FROM Bookmark b WHERE b.userId = :userId and b.questionId=:questionId")
+        @NamedQuery(name = "Bookmark.unbookmark", query = "DELETE FROM Bookmark b WHERE b.userId = :userId and b.questionId=:questionId"),
+        @NamedQuery(name = "Bookmark.getPagesCountBookmarked", query = "SELECT Count(b) FROM Bookmark b WHERE b.userId = :userId")
+
 })
 
 @Entity
