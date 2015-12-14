eaeabf2d656f202389f9b1392fa8316272efe490
Dmytro_Korniienko1 <Dmytro_Korniienko1@EPUAKYIW0130.kyiv.epam.com>
2015-12-14 17:36:02 +0200

    Added Table annotation to Comment and Like entities to match current table names in the database

--- src/main/java/ua/f13group/KnowHub/domain/Comment.java
+++ src/main/java/ua/f13group/KnowHub/domain/Comment.java
@@ -10,9 +10,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
-
+@Table(name = "comments")
 public class Comment {
 
 	@Id
