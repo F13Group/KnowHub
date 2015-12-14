800f3f68d6709b8f49dd8d6f1757bd08c100c54c
Oleg Kuts <kuts.oleg91@gmail.com>
2015-12-14 19:05:16 +0200

    Scroll commit

--- src/main/java/ua/f13group/KnowHub/web/rest/SingleQuestionPageController.java
+++ src/main/java/ua/f13group/KnowHub/web/rest/SingleQuestionPageController.java
@@ -1,11 +1,15 @@
 package ua.f13group.KnowHub.web.rest;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ua.f13group.KnowHub.domain.Question;
@@ -16,48 +20,107 @@ import ua.f13group.KnowHub.service.UserService;
 import ua.f13group.KnowHub.web.dto.QuestionFrequentAskedDTO;
 
 @RestController
-@RequestMapping(value = "/question/{questionId}/info")
+@RequestMapping(value = "/question")
 public class SingleQuestionPageController {
 
-    @Autowired
-    QuestionService questionService;
-    
-    @Autowired
-    UserService userService;
-    
-    @Autowired
-    RatingService ratingService;
+	@Autowired
+	QuestionService questionService;
 
-    @Autowired
-    BookmarkService bookmarkService;
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public QuestionFrequentAskedDTO getQuestionInfo(
-			@PathVariable Long questionId,
-			Principal principal) {
-		
-        Long userId = Long.valueOf(0);
-        if (principal != null) {
-            String login = principal.getName();
-            userId = userService.getUserByLogin(login).getUserId();
-        }
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	RatingService ratingService;
+
+	@Autowired
+	BookmarkService bookmarkService;
+
+	@RequestMapping(value = "/{questionId}/info", method = RequestMethod.GET)
+	public QuestionFrequentAskedDTO getQuestionInfo(@PathVariable Long questionId, Principal principal) {
+
+		Long userId = Long.valueOf(0);
+		if (principal != null) {
+			String login = principal.getName();
+			userId = userService.getUserByLogin(login).getUserId();
+		}
 
 		Boolean isBookmarked = bookmarkService.isBookmarked(userId, questionId);
-        Question curQuestion = questionService.getQuestionById(questionId);		
-        Long rating = ratingService.countLikesByQuestionId(questionId);
-        
-		return new QuestionFrequentAskedDTO(
-				curQuestion.getId(),
-				curQuestion.getValue(),
-				curQuestion.getLoadDate(),
-				curQuestion.getCategory(),
-				curQuestion.getTags(),
-				rating,
-				//ifLiked means wasAsked
-				ratingService.ifLiked(userId, questionId),
-				isBookmarked,
-				curQuestion.getUser(),
-				curQuestion.getViews(),
-				curQuestion.getDescription());		
+		Question curQuestion = questionService.getQuestionById(questionId);
+		Long rating = ratingService.countLikesByQuestionId(questionId);
+
+		return new QuestionFrequentAskedDTO(curQuestion.getId(), curQuestion.getValue(), curQuestion.getLoadDate(),
+				curQuestion.getCategory(), curQuestion.getTags(), rating,
+				// ifLiked means wasAsked
+				ratingService.ifLiked(userId, questionId), isBookmarked, curQuestion.getUser(), curQuestion.getViews(),
+				curQuestion.getDescription());
+	}
+
+	@RequestMapping(value = "/{questionId}/comments", method = RequestMethod.GET)
+	public List<CommentStub> getComments(@PathVariable Long questionId,
+			@RequestParam(value = "startPoint", required = false, defaultValue = "0") Integer startPoint,
+			@RequestParam(value = "commentsQuantity", required = false, defaultValue = "5") Integer commentsQuantity) {
+		List<CommentStub> commentStubs = new ArrayList<>(19);
+		for (int i = 0; i < 19; i++) {
+			commentStubs.add(new CommentStub(startPoint, commentsQuantity));
+		}
+		int commentsToLoad = (startPoint + commentsQuantity) > 19 ? 19 : (startPoint + commentsQuantity);
+		return new ArrayList<>(commentStubs.subList(startPoint, commentsToLoad));
+	}
+
+	private class CommentStub {
+		private String author;
+		private String value;
+		private Date date;
+		private Integer startPoint;
+		private Integer commentsQuantity;
+
+		public CommentStub(Integer startPoint, Integer commentsQuantity) {
+			super();
+			this.author = "Cat Ipsum";
+			this.value = "Cat ipsum dolor sit amet, make meme, make cute face yet scratch the furniture or eat and than sleep on your face. Chew iPad power cord chase laser love to play with owner's hair tie, and claws in your leg so intently stare at the same spot, for use lap as chair. Scream at teh bath the dog smells bad shake treat bag, yet my left donut is missing, as is my right. Meow stare at wall turn and meow stare at wall some more meow again continue staring so put toy mouse in food bowl run out of litter box at full speed yet intently sniff hand. Loves cheeseburgers stretch, for climb a tree, wait for a fireman jump to fireman then scratch his face. Sleep on keyboard. Brown cats with pink ears spot something, big eyes, big eyes, crouch, shake butt, prepare to pounce roll on the floor purring your whiskers off, hiss at vacuum cleaner for present belly, scratch hand when stroked kitty power! ";
+			this.date = new Date();
+			this.startPoint = startPoint;
+			this.commentsQuantity = commentsQuantity;
+		}
+
+		public String getAuthor() {
+			return author;
+		}
+
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+		public Date getDate() {
+			return date;
+		}
+
+		public void setDate(Date date) {
+			this.date = date;
+		}
+
+		public Integer getCommentsQuantity() {
+			return commentsQuantity;
+		}
+
+		public Integer getStartPoint() {
+			return startPoint;
+		}
+
+		public void setCommentsQuantity(Integer commentsQuantity) {
+			this.commentsQuantity = commentsQuantity;
+		}
+
+		public void setStartPoint(Integer startPoint) {
+			this.startPoint = startPoint;
+		}
 	}
 }
--- src/main/webapp/resources/js/singleQuestion.js
+++ src/main/webapp/resources/js/singleQuestion.js
@@ -2,6 +2,14 @@
  * 
  */
 var globalPageUrl = window.location.href.toString();
+var globalAppUrl = window.location.href.toString();
+var cutPoint = globalAppUrl.toLowerCase().indexOf("/knowhub/");
+globalAppUrl = globalAppUrl.substr(0, cutPoint + "/knowhub/".length);
+var infoUrl = globalPageUrl+"/info";
+var commentUrl = globalPageUrl + "/comments";
+var startPoint = 0;
+var commentsQuantityToLoad = 5;
+var noMoreItemsAvailable = false;
 
 var getCorrectUserLoginName = function(userName){
 	if (userName) {
@@ -13,15 +21,16 @@ var getCorrectUserLoginName = function(userName){
 }
 
 $(document).ready(function () {
-
 	//Output UserName for signed student
     var userName = $("#userName").html();
     $("#userName").empty();
     $("#userName").append(getCorrectUserLoginName(userName));
     
-    infoUrl = globalPageUrl+"/info";
+
     
     $.getJSON(infoUrl, function (data) {
+    	//Add 5 comments when page has been loaded
+    		loadComments();
     		$("#qTitle").append(data.value);
     		if (userName) {
     			$("#wasAsked").append(wasAskedButton(data.id, data.isAsked) + " ");
@@ -39,23 +48,21 @@ $(document).ready(function () {
     		if (data.description) {
     			$("#qDescription").append(data.description);
     		}
-    		
-    		//Output formatted date		
-    		var unformatted_date = new Date(data.loadDate);
-            var dd = unformatted_date.getDate();
-            var mm = unformatted_date.getMonth() + 1;
-            var yyyy = unformatted_date.getFullYear();
-            if (dd < 10) {
-                dd = '0' + dd;
-            }
-            if (mm < 10) {
-                mm = '0' + mm;
-            }
-            var date = dd + '/' + mm + '/' + yyyy;    		
-    		
-    		$("#addedDate").append("Added "+date);		
+    			   		
+    		$("#addedDate").append("Added "+formatDate(data));		
     		
     })
+
+	//infinite scroll
+	$(window).scroll(
+			function() {
+				if ($(window).scrollTop() >= ($(document)
+						.height() - $(window).height()) && !noMoreItemsAvailable) {
+					//load new portion
+					loadComments();
+				}
+			});
+   
 })
 
 function showTags(listOfTags){
@@ -64,4 +71,61 @@ function showTags(listOfTags){
 		res+="<a href='#'>"+ listOfTags[i].value + "</a> ";
 	}
 	return res;
+}
+
+function loadComments() {
+	//Sends index from where to start load comments and quantity.
+	 $.getJSON(commentUrl + '?startPoint=' + startPoint + '&commentsQuantity=' + commentsQuantityToLoad, function (data){
+		 /*
+		  * JSON lengths we get, is shorter than quantity we requested for - it means
+		  * that we've loaded last set of comments and we must not send any more request.
+		  * */
+		 if(data.length < commentsQuantityToLoad){
+			 noMoreItemsAvailable = true;
+		 }
+	    	for(var i = 0; i < data.length; i++){
+	    		var html =
+	    		'<div id="comment'+ ++startPoint +'" class=" col-lg-10 col-md-10 col-sm-10">\
+					<div class="row">\
+					  <div class="col-lg-2 col-md-2 col-sm-2 divCell_Left">\
+					    <img src="'+globalAppUrl+'/resources/img/account.png" width="20" height="20" /> <a href="#">'+data[i].author +'</a><br>\
+					    Added start index '+data[i].startPoint+' quantity '+ data.length+'\\'+data[i].commentsQuantity+'\
+				      </div>\
+					    \
+					  <div class="col-lg-10 col-md-10 col-sm-10 justified">'+ data[i].value+'\
+	                  </div>\
+					</div>\
+				</div>';
+				var divider ='<div class="col-lg-10 col-md-10 col-sm-10"><div class="row">\
+					<br>\
+					</div></div>';   
+	    		$("#userComments").append(html + divider);
+	    	}
+
+	    	/*
+	    	 * Adds some extra space to place the footer, when we reach the bottom 
+	    	 * (in order not to shadow last comment)
+	    	 */
+    		if(noMoreItemsAvailable){
+    			var footerHeight = $("footer").height();
+    			console.log(footerHeight);
+    			$("#userComments").append('<div class="col-lg-10 col-md-10 col-sm-10"><div class="row" style="height: '+footerHeight+'px;"></div></div>');
+    		}
+	    	 
+	    });	
+}
+
+function formatDate(data){
+	var unformatted_date = new Date(data.loadDate);
+    var dd = unformatted_date.getDate();
+    var mm = unformatted_date.getMonth() + 1;
+    var yyyy = unformatted_date.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    var date = dd + '/' + mm + '/' + yyyy; 
+    return date;
 }
